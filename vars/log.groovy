e54c163c8c699f3d3c253a770fa270b246f88004
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-03-22 15:37:37 +0530

    new commit

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,29 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
+//def info(message) {
+//    echo "INFO: ${message}"
+//}
+//
+//def warning(message) {
+//    echo "WARNING: ${message}"
+//}
 
-def warning(message) {
-    echo "WARNING: ${message}"
+def call() {
+    pipeline {
+        agent any
+        stages {
+          stage ( 'one'){
+              steps {
+                  sh 'echo One'
+
+              }
+          }
+          stage {
+              stage ( 'Two'){
+                  steps{
+                      sh 'echo Two'
+                  }
+              }
+          }
+        }
+
+    }
 }
