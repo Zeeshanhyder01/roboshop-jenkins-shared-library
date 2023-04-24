cddd693b09c420da5a69318f8e14bc32db9caa69
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-04-24 10:31:24 +0530

    new commit

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,37 +6,37 @@
 //    echo "WARNING: ${message}"
 //}
 
-def call(){
-    pipeline {
-        agent any
-        stages {
-            stage( 'one'){
-                steps {
-                    sh 'echo One ${COMPONENT}'
-
-                }
-            }
-            stage( 'Two'){
-                steps {
-                    sh 'echo Two ${COMPONENT}'
-
-                }
-            }
-        }
-    }
-}
-
-//def call() {
-//    node() {
-//        stage('one') {
-//            sh 'echo One - ${COMPONENT}'
-//        }
+//def call(){
+//    pipeline {
+//        agent any
+//        stages {
+//            stage( 'one'){
+//                steps {
+//                    sh 'echo One ${COMPONENT}'
 //
-//        //
-//        // THIS IS SAMPLE
+//                }
+//            }
+//            stage( 'Two'){
+//                steps {
+//                    sh 'echo Two ${COMPONENT}'
 //
-//        stage ('Two ') {
-//            sh 'echo Two - ${COMPONENT}'
+//                }
+//            }
 //        }
 //    }
 //}
+
+def call() {
+    node() {
+        stage('one') {
+            sh 'echo One - ${COMPONENT}'
+        }
+
+        //
+        // THIS IS SAMPLE
+
+        stage ('Two ') {
+            sh 'echo Two - ${COMPONENT}'
+        }
+    }
+}
