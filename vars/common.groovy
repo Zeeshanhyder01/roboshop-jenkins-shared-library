027c130007c2a393657d3e0bf3f118494ea65f35
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-04-24 16:56:57 +0530

    new commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -18,7 +18,7 @@ def publishArtifacts(){
         }
         if(env.APP_TYPE == "maven" ){
             sh """
-                cp target/${COMPONENT}-1.0.1.jar ${COMPONENT}.jar
+                cp target/${COMPONENT}-1.0.jar ${COMPONENT}.jar
        zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar   
     """
 
