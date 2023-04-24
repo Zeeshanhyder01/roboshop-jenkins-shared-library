5c60e66d46eb734cb43ec686862d0dda868f6331
Zeeshuzeeshu <zeeshuzeeshu110@gmail.com>
2023-04-24 17:26:04 +0530

    new commit

--- vars/common.groovy
+++ vars/common.groovy
@@ -25,7 +25,7 @@ def publishArtifacts() {
         }
         if (env.APP_TYPE == "python") {
             sh """
-              zip -r ${COMPONENT}-${TAG_NAME}.zip  *.py  ${COMPONENT}.ini requirements.txt 
+              zip -r ${COMPONENT}-${TAG_NAME}.zip  * .py ${COMPONENT}.ini requirements.txt 
             """
 
         }
